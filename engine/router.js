d327044b1d99af7f4e4bce5ce800c202b16fb0dc
Let_Null <alexvolkovdd@gmail.com>
2021-03-20 22:25:19 +0330

    Version 0.1

--- engine/router.js
+++ engine/router.js
@@ -1,9 +1,18 @@
-export class Router 
-{
-
-    __constructor() 
-    {
-        console.log("Router is created!");
-    }
-
+exports.Route = function(app) {
+    app.get(["/main", "/", "/m"], (req, res) => {
+        res.render('index', { title: "Anonimo main page" });
+    });
+    
+    app.post(["/main", "/", "/m"], (req, res) => {
+      console.log("User is log as: " + req.body.user.name);
+      req.session.nickname = req.body.user.name;
+      res.redirect('/list');
+    });
+    
+    app.get(["/list", "/l"], (req, res) => {
+      if (req.session.nickname != null)
+      {
+        res.render('list', { title: "Anonimo chat list", nickname: req.session.nickname});
+      } else res.redirect("/m");
+    });
 }
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
+var route = require('./engine/router');
+
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,22 +18,7 @@ app.use(session({secret: '734e987accf23', saveUninitialized: true,}));
  * ROUTING 
  */
 
-app.get(["/main", "/", "/m"], (req, res) => {
-    res.render('index', { title: "Anonimo main page" });
-});
-
-app.post(["/main", "/", "/m"], (req, res) => {
-  console.log("User is log as: " + req.body.user.name);
-  req.session.nickname = req.body.user.name;
-  res.redirect('/list');
-});
-
-app.get(["/list", "/l"], (req, res) => {
-  if (req.session.nickname != null)
-  {
-    res.render('list', { title: "Anonimo chat list", nickname: req.session.nickname});
-  } else res.redirect("/m");
-});
+route.Route(app);
 
 /*
  * SOCKET.IO LISTEN EVENTS
